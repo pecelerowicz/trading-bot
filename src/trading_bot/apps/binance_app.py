ba281b015904072cb2ac74ec2c98ecd61ec5b59b
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2026-05-06 13:40:15 +0200

    Add trading_session

--- src/trading_bot/apps/binance_app.py
+++ src/trading_bot/apps/binance_app.py
@@ -3,6 +3,7 @@ from binance import AsyncClient, BinanceSocketManager
 from trading_bot.handlers import handle_message_print
 
 from trading_bot.mappers.ws_kline_mapper import map_ws_kline
+from trading_bot.trading.trading_session import TradingSession
 
 
 class BinanceStreamer:
@@ -11,7 +12,7 @@ class BinanceStreamer:
         self.symbol = symbol
         self.interval = interval
 
-    async def stream(self, message_handler):
+    async def stream(self, trading_session: TradingSession):
         bm = BinanceSocketManager(self.async_client)
         ts = bm.kline_socket(symbol=self.symbol, interval=self.interval)
 
@@ -19,17 +20,18 @@ class BinanceStreamer:
             while True:
                 raw_msg = await stream.recv()
                 msg = map_ws_kline(raw_msg)
-                should_stop = await message_handler(msg)
+                should_stop = await trading_session.handle_kline(msg)
                 if should_stop:
                     break
 
 
 class BinanceApp:
-    def __init__(self, api_key, api_secret, symbol, interval):
+    def __init__(self, api_key, api_secret, symbol, interval, trading_session: TradingSession):
         self.api_key = api_key
         self.api_secret = api_secret
         self.symbol = symbol
         self.interval = interval
+        self.trading_session = trading_session
 
     async def run(self):
         async_client = await AsyncClient.create(
@@ -40,6 +42,6 @@ class BinanceApp:
 
         try:
             streamer = BinanceStreamer(async_client, self.symbol, self.interval)
-            await streamer.stream(handle_message_print)
+            await streamer.stream(trading_session=self.trading_session)
         finally:
             await async_client.close_connection()
--- src/trading_bot/main.py
+++ src/trading_bot/main.py
@@ -4,6 +4,7 @@ import asyncio
 
 from trading_bot.apps.binance_app import BinanceApp
 from trading_bot.apps.mock_app import MockApp
+from trading_bot.trading.trading_session import TradingSession
 
 load_dotenv()
 
@@ -15,11 +16,15 @@ use_mock = os.getenv("USE_MOCK", "false").lower() == "true"
 async def main():
     print("Hello trading bot")
 
+    trading_session = TradingSession()
+
     if use_mock:
         # TODO it should throw an exception if the data range is not within what is available in the raw data
-        app = MockApp(symbol='BTCUSDT', interval="1h", initial_date="2023-01-01", final_date="2024-01-02", delay_seconds=1.0)
+        app = MockApp(symbol='BTCUSDT', interval="1h", initial_date="2023-01-01", final_date="2024-01-02",
+                      delay_seconds=1.0, trading_session=trading_session)
     else:
-        app = BinanceApp(api_key=api_key, api_secret=api_secret, symbol="BTCUSDT", interval='1h')
+        app = BinanceApp(api_key=api_key, api_secret=api_secret, symbol="BTCUSDT", interval='1h',
+                         trading_session=trading_session)
 
     await app.run()
 
